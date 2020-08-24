5ca4d03b5358bf7bb753f6a64cff2b4eeebd39f9
parthgundi <parthgundi99@gmail.com>
2020-08-24 14:18:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2FzaGVsbCIsImEiOiJja2Ryaml2bzQwOWloMnlvN2htdXdra2c3In0.eZwWsYuro0bwKk16bScIbw';
 var map = new mapboxgl.Map({
 style: 'mapbox://styles/mapbox/light-v10',
@@ -561,7 +562,7 @@ map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
  
 map.addSource('Social Impact Housing', {
 'type': 'geojson',
-'data': 'https://services5.arcgis.com/mv4wB4q9dMbOaZn3/ArcGIS/rest/services/reall_data_explorer_2020_08_03_11_02_40/FeatureServer/0/query?where=ObjectID%3E0&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=ilJm9UoBP39uHQzJw_k0YLapVz20TqtblKn384JZLV2bMkYl7EU3i_PBPPx8rSfOwXr9652Y5NxBJCsGv7oXIKWun0kqGo_EC_xqRi9n1DLeU-KrnXOIjWP_m3_vW7f3Z6KiwdvHfnhl2kEhiCxYalqbbuSezIgif0r2s5PRM07Y4ywHaktomVoWQb_CXJCPJ6j-E2NqGzWUFdXpgglMVUtrPORgPhw3571DVfQQgzA.'
+'data': 'https://services5.arcgis.com/mv4wB4q9dMbOaZn3/arcgis/rest/services/reall_data_explorer_2020_08_03_11_02_40/FeatureServer'
 });
 map.addLayer({
 'id': 'Social Impact Housing',
@@ -597,6 +598,73 @@ map.getCanvas().style.cursor = 'pointer';
 // Change it back to a pointer when it leaves.
 map.on('mouseleave', 'places', function() {
 map.getCanvas().style.cursor = '';
+});
+});
+
+map.on('load', function() {
+map.addSource('states', {
+'type': 'geojson',
+'data':
+'https://services5.arcgis.com/mv4wB4q9dMbOaZn3/ArcGIS/rest/services/admin1/FeatureServer/0/query?where=ObjectID%3E0&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=MVq-mZKi7H3IG90i7rjL2dmv_v82LzBNwwHVZ5dSBbCEnO49uErhXd0NpVQOoPrwL6MSiMWpwqe9TUyGuFI7FO-_getFIZFG9IzyW0UppxIefijgAg2wj9taxzdGXutXt8W3oOtC3fOrrhkTzRv0hCydM2FQsbwzyQ76eLLOFdadtZnZocuK6sp_fNXUDYyUoHEPtJ1oR2Fr89v9GtBhuBl91w3IaRhNba-jE8654X0.'
+});
+ 
+// The feature-state dependent fill-opacity expression will render the hover effect
+// when a feature's hover state is set to true.
+map.addLayer({
+'id': 'state-fills',
+'type': 'fill',
+'source': 'states',
+'layout': {},
+'paint': {
+'fill-color': '#627BC1',
+'fill-opacity': [
+'case',
+['boolean', ['feature-state', 'hover'], false],
+1,
+0.5
+]
+}
+});
+ 
+map.addLayer({
+'id': 'state-borders',
+'type': 'line',
+'source': 'states',
+'layout': {},
+'paint': {
+'line-color': '#627BC1',
+'line-width': 2
+}
+});
+ 
+// When the user moves their mouse over the state-fill layer, we'll update the
+// feature state for the feature under the mouse.
+map.on('mousemove', 'state-fills', function(e) {
+if (e.features.length > 0) {
+if (hoveredStateId) {
+map.setFeatureState(
+{ source: 'states', id: hoveredStateId },
+{ hover: false }
+);
+}
+hoveredStateId = e.features[0].id;
+map.setFeatureState(
+{ source: 'states', id: hoveredStateId },
+{ hover: true }
+);
+}
+});
+ 
+// When the mouse leaves the state-fill layer, update the feature state of the
+// previously hovered feature.
+map.on('mouseleave', 'state-fills', function() {
+if (hoveredStateId) {
+map.setFeatureState(
+{ source: 'states', id: hoveredStateId },
+{ hover: false }
+);
+}
+hoveredStateId = null;
 });
 });
 
